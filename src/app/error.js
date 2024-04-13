bb353f60aa280598781cfa4cd54ec48b905879b0
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-04-14 01:10:16 +0530

    error handling

---
+++ src/app/error.js
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+
+function error({ error, reset }) {
+  React.useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  return (
+    <div className="text-center mt-10">
+      <h1 className="mb-2">Something went wrong, please try again</h1>
+      <button
+        className="py-1 px-2 dark:bg-blue-600 rounded-lg
+         dark:text-white bg-blue-500 text-gray-300"
+        onClick={() => reset()}
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
+
+export default error;
--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,13 @@ import axios from "axios";
 
 export default async function Home({ searchParams }) {
   let results = []
-  try {
-    const genre = searchParams.genre || 'fetchTrending'
-    const res = await axios.get(
-      `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-      }?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`,
-      { next: { revalidate: 10000 } }
-    ).then((response) => response.data)
-    results = res.results
-  } catch (error) {
-    console.log(error);
-  }
+  const genre = searchParams.genre || 'fetchTrending'
+  const res = await axios.get(
+    `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  ).then((response) => response.data)
+  results = res.results
 
   return (
     <div>
