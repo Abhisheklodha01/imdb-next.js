3a9ec299554d73a46b41afaa0f0bb77ed3df94cb
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-04-13 22:35:14 +0530

    adding dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,24 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "IMDB: Home",
-  
+
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
-        </body>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,28 @@
-"use clinet"
-import {AiFillHome} from 'react-icons/ai'
-import {BsFillInfoCircleFill} from 'react-icons/bs'
-import MenuItme from "./MenuItme"
-import Link from 'next/link'
+"use clinet";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import MenuItme from "./MenuItme";
+import Link from "next/link";
+import SwitchTheme from "./SwitchTheme";
 
 function Header() {
   return (
-    <nav className='bg-gray-700 py-4 flex justify-between items-center p-3'>
-        <div className='flex gap-4 ml-10'>
-          <MenuItme
-          title ="Home"
-          address = "/"
-          Icon = {AiFillHome}
-          />
-          <MenuItme
-          title ="About"
-          address = "/about"
-          Icon = {BsFillInfoCircleFill}
-          />
-        </div>
-        <Link 
-        href={"/"}
-        className='flex gap-1 items-center'>
-          <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDb</span>
-          <span className='text-xl hidden sm:inline'>Clone</span>
+    <div className=" bg-cyan-700 py-4 flex justify-between items-center p-3">
+      <div className="flex gap-4 ml-10">
+        <MenuItme title="Home" address="/" Icon={AiFillHome} />
+        <MenuItme title="About" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+      <div className="flex gap-8 items-center">
+        <SwitchTheme />
+        <Link href={"/"} className="flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
+            IMDb
+          </span>
+          <span className="text-xl hidden sm:inline">Clone</span>
         </Link>
-    </nav>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
