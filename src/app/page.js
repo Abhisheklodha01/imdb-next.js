d9ab7699c6d4aecb53888d4b0b04429719bb44a3
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-04-14 17:18:29 +0530

    adding movie details page

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,14 @@ export default async function Home({ searchParams }) {
   const genre = searchParams.genre || 'fetchTrending'
   const res = await fetch(
     `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`,
+    }?api_key=${process.env.TMDB_KEY}&language=hi-IN&page=2`,
   )
   const data = await res.json();
   if (!res.ok) {
     throw new Error('Failed to fetch data');
   }
   const results = data.results;
+  console.log(results);
   return (
     <div>
       <Results results={results} />
